12ff2ce50e9adef752f01039116640607499fe06
Karan Markandey <karan.iitg54@gmail.com>
2023-07-28 11:12:52 +0530

    adding like and share features

--- app/src/main/java/com/example/jokesapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/jokesapp/ViewPagerAdapter.kt
@@ -1,19 +1,47 @@
 package com.example.jokesapp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jokesapp.databinding.ActivityMainBinding
 import com.example.jokesapp.databinding.JokeLayoutBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.JokeViewHolder>() {
     private  var jokesList  = ArrayList<Joke>()
+
 
     inner class JokeViewHolder( val binding: JokeLayoutBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(joke: Joke ){
             binding.tvCategory.text = "-${joke.category}"
             binding.tvSetup.text = joke.setup
             binding.tvDelivery.text = joke.delivery
+            var clickCount  =0
+
+            binding.root.setOnClickListener(){
+                if(clickCount !=2 ){
+                    clickCount++
+                }
+                else{
+                    clickCount=0
+                    binding.likeanim.apply {
+                        visibility = View.VISIBLE
+                        playAnimation()
+                        CoroutineScope(Dispatchers.Default).launch{
+                            delay(1800)
+                            withContext(Dispatchers.Main){
+                                binding.likeanim.visibility = View.GONE
+                            }
+                        }
+                    }
+                }
+            }
+
         }
     }
 
@@ -29,6 +57,7 @@ class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.JokeViewHolder>()
     override fun onBindViewHolder(holder: JokeViewHolder, position: Int) {
         holder.bind(jokesList[position])
         holder.binding.tvPosition.text = (position+1).toString()
+
     }
 
     fun addJokes(jokes: List<Joke>){
